9f761312cae73f955bf526024dde91bd0ddd7a19
Shlokmatha <103404603+Shlokmatha@users.noreply.github.com>
2022-04-24 14:52:46 +0530

    Add files via upload

---
+++ QUES3.py
@@ -0,0 +1,7 @@
+name = input('NAME: ')
+father_name = input('FATHER NAME: ')
+mother_name = input('MOTHER NAME: ')
+age = input('YOU AGE: ')
+blood = input('BLOOD GROUP: ')
+L = [name, father_name, mother_name, age, blood]
+print(L)
