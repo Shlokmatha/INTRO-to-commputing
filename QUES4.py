10812908a3f9f3fe3908b9f661dc6e1584bda306
Shlokmatha <103404603+Shlokmatha@users.noreply.github.com>
2022-04-24 14:53:08 +0530

    Add files via upload

---
+++ QUES4.py
@@ -0,0 +1,9 @@
+stu1 = int(input('M1: '))
+stu2 = int(input('M2: '))
+stu3 = int(input('M3: '))
+stu4 = int(input('M4: '))
+stu5 = int(input('M5: '))
+L = [stu1, stu2, stu3, stu4, stu5]
+L.sort()
+# We can't directly write print(l.sort()) as it will result NONE
+print(L)
